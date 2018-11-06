6c9bc35ce163af60fd287e99d2e7eb3c2389a53b
Noah <noah@jeff.org>
2018-11-06 02:55:52 -0500

    use heroku port instead of static 5000

--- app.py
+++ app.py
@@ -65,12 +65,11 @@ def search():
 # Launch
 
 # Default port:
-if __name__ == '__main__':
-    app.run()
+#if __name__ == '__main__':
+#    app.run()
 
 # Or specify port manually:
-'''
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-'''
